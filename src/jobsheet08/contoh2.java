532bf9aad4beb3663726f7a3fd1a069d625b604c
Acer <Acer@DESKTOP-S8USU8H>
2018-08-02 08:20:25 +0700

    FOR WHILE DO-WHILE

---
+++ src/jobsheet08/contoh2.java
@@ -0,0 +1,22 @@
+
+package jobsheet08;
+public class contoh2 {
+    public static void main(String[] args){
+    int x;
+    for(x = 1;x<=10;x++)
+    {System.out.println(x);}
+    
+    //pengulangan WHILE
+        int i = 11;
+        while(i <= 20)
+        {
+        System.out.println(i +"");
+        i++;}
+        
+        //pengulangan DO WHILE
+        int j = 21;
+        do
+        {System.out.println(j ); j++;}
+        while(j <= 30);
+    }
+}
