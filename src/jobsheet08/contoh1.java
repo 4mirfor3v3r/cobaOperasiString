39a4ee4bb762cd529c5d752db04de35ab8c8ec3b
Acer <Acer@DESKTOP-S8USU8H>
2018-08-02 07:47:07 +0700

    FOR WHILE DO-WHILE

---
+++ src/jobsheet08/contoh1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet08;
+
+/**
+ *
+ * @author Acer
+ */
+public class contoh1 {
+
+    /**
+     
+     */
+    public static void main(String[] args) {
+       
+        //pengulangan FOR
+        int x ;
+        for(x = 1; x <= 5; x++)
+        {
+        System.out.println(x +" SMK Telkom Malang");
+        }
+        //pengulangan WHILE
+        int i = 7;
+        while(i <= 12)
+        {
+        System.out.println(i +" SMK Telkom Malang");
+        i++;}
+        //pengulangan DO WHILE
+        int j = 13;
+        do
+        {System.out.println(j +"SMK Telkom Malang"); j++;}
+        while(j <= 18);
+    }
+    
+}
