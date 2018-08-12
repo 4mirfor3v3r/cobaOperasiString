f08b1c6af31cb4f8328fbff707f9888b88f61086
Acer <Acer@DESKTOP-S8USU8H>
2018-08-12 14:35:10 +0700

    Coba method length&Empty

--- src/j11/cobaOprerasiString.java
+++ src/j11/cobaOprerasiString.java
@@ -5,6 +5,15 @@ public class cobaOprerasiString {
     String identitas = "Ahmad Amirudin / X-RPL3 / 02";
         System.out.println("Identitas : "+ identitas);
         
+        String x = "Operasi";
+        System.out.println("isi variabel x : " + x);
+        System.out.println("\""+ x +"\"Panjang Karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variabel y : "+ y);
+        System.out.println("y adalah Kosong : "+ y.isEmpty());
         
     }
 }
