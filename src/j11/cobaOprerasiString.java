8dffaab52bad81b840b7822a504fb9c83dae3332
Acer <Acer@DESKTOP-S8USU8H>
2018-08-12 13:01:02 +0700

    Initial Commit

---
+++ src/j11/cobaOprerasiString.java
@@ -0,0 +1,10 @@
+package j11;
+
+public class cobaOprerasiString {
+    public static void main(String[] argas){
+    String identitas = "Ahmad Amirudin / X-RPL3 / 02";
+        System.out.println("Identitas : "+ identitas);
+        
+        
+    }
+}
