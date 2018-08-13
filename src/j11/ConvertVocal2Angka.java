834dbcf4ef11f169bc14083bc7da21fbfa7c7240
Acer <Acer@DESKTOP-S8USU8H>
2018-08-13 08:20:51 +0700

    Initial Commit

---
+++ src/j11/ConvertVocal2Angka.java
@@ -0,0 +1,15 @@
+package j11;
+
+public class ConvertVocal2Angka {
+    public static void tampilJudul(String identitas){
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    private static void main(String[] args){
+    String identitas = "Ahmad Amirudin / X-RPL3 / 02";
+    
+    tampilJudul(identitas);
+    
+    }
+}
