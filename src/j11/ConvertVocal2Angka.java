a4c31a08141c7338af28c963e533605834d608cb
Acer <Acer@DESKTOP-S8USU8H>
2018-08-14 04:20:11 +0700

    Tambah Konversi

--- src/j11/ConvertVocal2Angka.java
+++ src/j11/ConvertVocal2Angka.java
@@ -6,12 +6,14 @@ public class ConvertVocal2Angka {
         
         System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
     }
-    private static void main(String[] args){
+    public static void main(String[] args){
     String identitas = "Ahmad Amirudin / X-RPL3 / 02";
     
     tampilJudul(identitas);
     
     String kalimat = tampilInput();
+    
+    String convert = vocal2Angka(kalimat);
     }
     private static String tampilInput(){
     Scanner scanner = new Scanner(System.in);
@@ -21,7 +23,17 @@ public class ConvertVocal2Angka {
         System.out.println("Kalimat Asli" + kalimat);
         
         return kalimat;
-    }
+        
+    } private static String vocal2Angka(String kalimat){
+        char [][] arConvert = {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+        
+        kalimat = kalimat.toLowerCase();
+        for(int i = 0;i<arConvert.length;i++){
+        kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+        }return kalimat;
+        }
+        
+    
     
     
 }
